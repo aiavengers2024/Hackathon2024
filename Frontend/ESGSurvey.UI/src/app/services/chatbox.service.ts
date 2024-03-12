3dc6698259f83627498c603069206af0fe9069d6
mkrishna074 <muralikrishna.korivi@gmail.com>
2024-03-12 19:29:50 +0530

    Sanitizing input

--- Frontend/ESGSurvey.UI/src/app/services/chatbox.service.ts
+++ Frontend/ESGSurvey.UI/src/app/services/chatbox.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Injectable, SecurityContext } from '@angular/core';
 import * as io from 'socket.io-client';
 import { Observable, Subject } from 'rxjs';
 import { Message } from '../models/Message';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ApiUrlConstants } from '../common/constants/apiUrl.constants';
+import { DomSanitizer } from '@angular/platform-browser';
 
 
 @Injectable({
@@ -12,20 +13,22 @@ import { ApiUrlConstants } from '../common/constants/apiUrl.constants';
 export class ChatboxService {
     conversation = new Subject<Message[]>();
   
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private sanitizer: DomSanitizer) {
     }
   
     getopenAIAnswer(msg: string) {
-      const userMessage = new Message('user', msg);  
+      const userMessage = new Message('user', this.sanitizer.sanitize(
+        SecurityContext.HTML,
+        msg
+      ));  
       this.conversation.next([userMessage]);
       this.getopenAIMessage(msg);
     }
   
     getopenAIMessage(question: string): any{
         const headers = new HttpHeaders({ 'content-type': 'application/json' });
-        const body = JSON.stringify(question);
-        console.log(body)
-        this.http.post(ApiUrlConstants.OpenAIServiceServicesTest + question, { 'headers': headers }).subscribe((response: any) =>{
+        this.http.post(ApiUrlConstants.OpenAIServiceServicesTest + this.sanitizer.sanitize(
+            SecurityContext.HTML, question), { 'headers': headers }).subscribe((response: any) =>{
             const openAIMessage = new Message('openai', response);
             this.conversation.next([openAIMessage]);
         },
