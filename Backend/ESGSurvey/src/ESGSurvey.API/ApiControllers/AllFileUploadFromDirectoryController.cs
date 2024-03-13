fa8cf9c63937c5c1397549ec2522f9dec4d1088a
pradhan prasanta <pradhan.prasanta78@gmail.com>
2024-03-14 00:14:52 +0530

    Upload files to blob from file and dir path

---
+++ Backend/ESGSurvey/src/ESGSurvey.API/ApiControllers/AllFileUploadFromDirectoryController.cs
@@ -0,0 +1,62 @@
+﻿using ESGSurvey.Data.BusinessObject;
+using ESGSurvey.Data.Core;
+using Microsoft.AspNetCore.Mvc;
+namespace ESGSurvey.API.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AllFileUploadFromDirectoryController : ControllerBase
+    {
+        #region Global Variable(s)
+        private readonly IConfigurationSettings _configuration;
+        private readonly IBlobServiceCore _blobServiceCore;
+        private readonly IAzureAISearchServicesCore _azureAISearchServicesCore;
+        #endregion
+
+        public AllFileUploadFromDirectoryController(IBlobServiceCore blobServiceCore, IAzureAISearchServicesCore azureAISearchServicesCore, IConfigurationSettings configuration)
+        {
+            _blobServiceCore = blobServiceCore;
+            _azureAISearchServicesCore = azureAISearchServicesCore;
+            _configuration = configuration;
+        }
+
+        [HttpPost(""), DisableRequestSizeLimit]
+        public async Task<IActionResult> UploadBlobs(string DirectoryPath)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(DirectoryPath))
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    string[] fileEntries = Directory.GetFiles(DirectoryPath);
+                    foreach (string fileName in fileEntries)
+                    {
+                        string FileName = Path.GetFileName(fileName);
+                        var fileStream = System.IO.File.OpenRead(fileName);
+                        if (fileStream == null)
+                        {
+                            return BadRequest();
+                        }
+                        else
+                        {
+                            var output = await _blobServiceCore.UploadFileBlobAsync(fileStream, FileName);
+                            if (output != null)
+                            {
+                                var isRunAndCheckIndexer = _azureAISearchServicesCore.RunAndCheckIndexer();
+                            }
+
+                        }
+                    }
+                    return Ok();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
